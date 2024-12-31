41260c488aedfc57b57b166369626a7bc93789f8
David Cooper <dmcooper2412@gmail.com>
2024-12-31 10:56:21 -0600

    re-organized layout, added step view

--- app/page.tsx
+++ app/page.tsx
@@ -68,21 +68,51 @@ interface Forecast {
 }
 
 interface LoadingProps {
-  isLoading: boolean;
   title: String;
-  children: React.ReactNode;
 }
 
-function Loading({isLoading, title, children}: LoadingProps) {
-  return (isLoading ? <div>Loading {title}</div> : children)
+function Loading({title}: LoadingProps) {
+  return (
+    <div className="flex">    
+      <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+      </svg>
+      <div className="p-2">Loading {title}</div>
+    </div>
+    )
+}
+
+interface ViewProps {
+  coords: Coordinate;
+  forecast: Forecast;
+}
+
+function View({coords, forecast}: ViewProps) {
+  return (
+    <div> 
+      <div>
+        Location: {coords.latitude}, {coords.longitude} 
+      </div>
+      <div>
+        <div>Forecast</div>
+        {forecast.timelines?.daily.map((day, index) => <div key={index}>{new Date(String(day.time)).getDate()} average {day.values.temperatureApparentAvg} C</div>)} 
+      </div>
+    </div>
+  )
+}
+
+enum STEP {
+  LOADING_COORDS,
+  LOADING_FORECAST,
+  LOADED_ALL
 }
 
 export default function Home() {
 
   const [coords, setCoords] = useState<Coordinate>(DEFAULT_COORDINATES);
-  const [isLoadingCoords, setLoadingCoords] = useState<boolean>(true);
+  const [step, setStep] = useState<STEP>(STEP.LOADING_COORDS);
   const [forecast, setForecast] = useState<Forecast>({});
-  const [isLoadingForecast, setLoadingForecast] = useState<boolean>(true);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -92,7 +122,7 @@ export default function Home() {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
           });
-          setLoadingCoords(false);
+          setStep(STEP.LOADING_FORECAST);
           const url = `https://api.tomorrow.io/v4/weather/forecast?location=${position.coords.latitude},${position.coords.longitude}&apikey=${process.env.NEXT_PUBLIC_WEATHER_API_TOKEN}`;
           const options = {method: 'GET', headers: {accept: 'application/json'}};
       
@@ -100,7 +130,7 @@ export default function Home() {
             .then(res => res.json())
             .then(json => setForecast(json))
             .catch(err => console.error(err))
-            .finally(() => setLoadingForecast(false));
+            .finally(() => setStep(STEP.LOADED_ALL));
         },
         (error) => {
           console.error('Error getting location:', error);
@@ -111,22 +141,33 @@ export default function Home() {
     }
   }, []);
 
+  const getView = (step: STEP) => {
+
+    let view = null;
+
+    switch(step){
+      case STEP.LOADING_COORDS:
+        view = <Loading title="Coords"/>
+        break;
+      case STEP.LOADING_FORECAST:
+        view = <Loading title="Forecast"/>
+        break;
+      case STEP.LOADED_ALL:
+        view = (
+            <View coords={coords} forecast={forecast} />
+        )
+        break;
+      default:
+        view = <div>Unable to determine view for step {step}.</div>
+    }
+
+    return view;
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div>
-          Weather App
-        </div>
-        <Loading isLoading={isLoadingCoords} title="Coordinates">
-          <div>
-            Current Location: {coords.latitude}, {coords.longitude}
-          </div>
-        </Loading>
-        <Loading isLoading={isLoadingForecast} title="Forecast">
-          <div>
-            {forecast.timelines?.daily.map((day, index) => <div key={index}>{new Date(String(day.time)).getDate()} average {day.values.temperatureApparentAvg} C</div>)}
-          </div>
-        </Loading>
+        {getView(step)}
        </main>
     </div>
   );
